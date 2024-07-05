5697c2748030146ae3c63065d651a81bd4306aa0
Laura <pizzuttilaura@gmail.com>
2024-07-05 12:26:20 -0300

    x

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Alterar from './components/Alterar';
 import Contador from './components/Contador';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Contador/> 
+        <Alterar/>
       </header>
     </div>
   );
